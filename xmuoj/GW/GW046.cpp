307c8608a53259180cdfc66dc0566111f6018d63
zeng8258 <3555182503@qq.com>
2024-09-20 23:15:59 +0800

    GW046 指针练习:输出Program

--- xmuoj/GW/GW046.cpp
+++ xmuoj/GW/GW046.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 using namespace std;
+
 void Print(const char * p1, const char * p2) 
 {  
+    // 题目似乎有问题，正常代码过不了
 	static int cnt = 0;
   	if (cnt == 0) printf("Program"), cnt++;
   	else printf("Pro");
