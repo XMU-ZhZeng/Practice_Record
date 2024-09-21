ded64cc739afa5ab06312eca9cc399a798bab17d
zeng8258 <3555182503@qq.com>
2024-09-21 22:51:30 +0800

    ACM850 Dijkstra求最短路(2)

---
+++ xmuoj/ACW/ACM850.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+#include <vector>
+#include <queue>
+#include <utility>
+
+#define PII pair<int, int>
+
+vector<PII> graph[150001];
+int dist[150001];
+int n, m;
+
+void dijkstra()
+{
+    queue<int> q;
+    q.push(1);
+
+    while (q.size())
+    {   int t = q.front();
+        vector<PII>& vt = graph[t];
+        q.pop();
+
+        for (auto i : vt)
+        {
+            if (dist[i.first] > dist[t] + i.second)
+            {
+                dist[i.first] = dist[t] + i.second;
+                q.push(i.first);
+            }
+        }
+    }
+}
+
+int main()
+{
+    cin >> n >> m;
+
+    for (int i = 0; i < m; i++)
+    {
+        int u, v, w;
+        cin >> u >> v >> w;
+        graph[u].push_back({v, w});
+    }
+
+    for (int i = 2; i <= n; i++) dist[i] = 0x3f3f3f3f;
+    dist[1] = 0;
+
+    dijkstra();
+
+    if (dist[n] == 0x3f3f3f3f) cout << -1 << endl;
+    else cout << dist[n] << endl;
+
+    return 0;
+}
