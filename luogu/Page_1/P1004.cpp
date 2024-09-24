46e798262b46b92544b91901e661a2abb64a8c1d
zeng8258 <3555182503@qq.com>
2024-09-24 22:29:55 +0800

    优化 P1004 [NOIP2000 提高组] 方格取数

--- luogu/Page_1/P1004.cpp
+++ luogu/Page_1/P1004.cpp
@@ -5,21 +5,20 @@ using namespace std;
 int main()
 {
     int N; cin >> N;
-    int matrix[11][11] = { 0 }, dp[11][11][11][11] = { 0 };
+    int matrix[11][11] = { 0 }, dp[11][11][22] = { 0 };
 
     int x, y, z;
     while (cin >> x >> y >> z && x && y && z) matrix[x][y] = z;
 
     for (int i = 1; i <= N; i++)
         for (int j = 1; j <= N; j++)
-            for (int k = 1; k <= i + j - 1; k++)
+            for (int k = max(i, j) + 1; k <= min(i, j) + N; k++)
                 {
-                    int q = i + j - k;
-                    dp[i][j][k][q] = max(max(dp[i-1][j][k-1][q], dp[i-1][j][k][q-1]), max(dp[i][j-1][k-1][q], dp[i][j-1][k][q-1])) + matrix[i][j] + matrix[k][q];
-                    if (i == k && j == q) dp[i][j][k][q] -= matrix[i][j];
+                    dp[i][j][k] = max(max(dp[i-1][j][k-1], dp[i-1][j-1][k-1]), max(dp[i][j-1][k-1], dp[i][j][k-1])) + matrix[k-i][i] + matrix[k-j][j];
+                    if (i == j) dp[i][j][k] -= matrix[k-i][i];
                 }
 
-    cout << dp[N][N][N][N] << endl;
+    cout << dp[N][N][2 * N] << endl;
 
     return 0;
 }
