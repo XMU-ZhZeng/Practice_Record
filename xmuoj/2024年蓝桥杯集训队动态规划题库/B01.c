101bf315de08f99202db6432305f4c8ed2034494
zeng8258 <3555182503@qq.com>
2024-10-22 19:10:54 +0800

    B01 多重背包问题(1)

---
+++ xmuoj/2024年蓝桥杯集训队动态规划题库/B01.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int vw[1010][3];
+int dp[1010][1010] = { 0 };
+
+int main()
+{
+    int N, V; scanf("%d%d", &N, &V);
+
+    for (int i = 1; i <= N; i++) scanf("%d%d%d", &vw[i][0], &vw[i][1], &vw[i][2]);
+
+    for (int i = 1; i <= N; i++)
+        for (int j = 1; j <= V; j++)
+        {
+            int k = 0;
+            while (k * vw[i][0] <= j && k <= vw[i][2])
+            {
+                dp[i][j] = dp[i][j] > dp[i - 1][j - k * vw[i][0]] + k * vw[i][1]? dp[i][j] : dp[i - 1][j - k * vw[i][0]] + k * vw[i][1];
+                k++;
+            }
+        }
+    printf("%d", dp[N][V]);
+
+    return 0;
+}
