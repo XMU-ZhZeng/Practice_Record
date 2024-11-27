4e3bea98d37e8a9ba24097e114c5d478cd96dcf7
zeng8258 <3555182503@qq.com>
2024-11-27 19:21:37 +0800

    DP26 装箱问题

---
+++ xmuoj/2024年蓝桥杯集训队动态规划题库/DP26.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int v, n;
+int vs[35];
+int dp[35][20010] = { 0 };
+int visited[35][20010] = { 0 };
+
+int DP(int idx, int volume)
+{
+    if (idx == 1) return (volume >= vs[1] ? volume - vs[1] : volume);
+    if (visited[idx][volume]) return dp[idx][volume];
+  
+    visited[idx][volume] = true;
+    if (volume < vs[idx]) return dp[idx][volume] = DP(idx - 1, volume);
+    return dp[idx][volume] = min(DP(idx - 1, volume - vs[idx]), DP(idx - 1, volume));
+}
+
+int main()
+{
+    cin >> v >> n;
+    for (int i = 1; i <= n; i++) cin >> vs[i];
+  
+    cout << DP(n, v);
+    
+    return 0;
+}
