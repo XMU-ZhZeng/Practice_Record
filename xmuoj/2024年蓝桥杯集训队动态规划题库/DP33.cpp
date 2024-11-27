29174ed8d76f2a5b27e3d94a7d68fd7a447f51aa
zeng8258 <3555182503@qq.com>
2024-11-27 22:56:35 +0800

    DP33 庆功会

---
+++ xmuoj/2024年蓝桥杯集训队动态规划题库/DP33.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int values[505][3];
+int dp[505][6010] = { 0 };
+bool visited[505][6010] = { 0 };
+
+int DP(int n, int v)
+{
+    if (!n) return 0;
+    if (visited[n][v]) return dp[n][v];
+    
+    visited[n][v] = true;
+  
+    for (int i = 0; (v >= i * values[n][0]) && (i <= values[n][2]); i++)
+        dp[n][v] = max(dp[n][v], DP(n - 1, v - i * values[n][0]) + i * values[n][1]);
+    return dp[n][v];
+}
+
+
+int main()
+{
+    int n, v; cin >> n >> v;
+    for (int i = 1; i <= n; i++) cin >> values[i][0] >>values[i][1] >> values[i][2];
+     
+    cout << DP(n, v);  
+  
+    return 0;
+}
